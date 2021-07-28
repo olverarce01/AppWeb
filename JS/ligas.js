2015733cf034da69bae7ed68858f2b6141558655
Ignacio <5360.com18@gmail.com>
2021-07-27 21:53:40 -0400

    actualizado

--- JS/ligas.js
+++ JS/ligas.js
@@ -80,3 +80,56 @@ i++;
 }
 }
 obtenerFechas();
+document.getElementById('botonNotificaciones').onclick=function(){
+  
+    // Comprobamos si el navegador soporta las notificaciones
+  if (!("Notification" in window)) {
+    alert("Este navegador no soporta las notificaciones del sistema");
+  }
+
+  // Comprobamos si ya nos habían dado permiso
+  else if (Notification.permission === "granted") {
+    // Si esta correcto lanzamos la notificación
+    var notification = new Notification("Ya aceptaste las notificaciones");
+  }
+
+  // Si no, tendremos que pedir permiso al usuario
+  else if (Notification.permission !== 'denied') {
+    Notification.requestPermission(function (permission) {
+      // Si el usuario acepta, lanzamos la notificación
+      if (permission === "granted") {
+        var notification = new Notification("Bienvenido a Notificaciones ChileanPremierLeague");
+      }
+    });
+  }
+
+  // Finalmente, si el usuario te ha denegado el permiso y
+  // quieres ser respetuoso no hay necesidad molestar más.
+   
+}
+function spawnNotification(theBody,theIcon,theTitle) {
+    var options = {
+        body: theBody,
+        icon: theIcon
+    }
+    var n = new Notification(theTitle,options);
+//setTimeout(n.close.bind(n), 5000);
+}
+//spawnNotification("a vs b","recursos/zee-ball.png","Partido de Hoy");
+var database=firebase.database();
+var commentsRef = firebase.database().ref('notificaciones');
+commentsRef.on('child_added', (data) => {
+  spawnNotification(data.val().body,data.val().icono,data.val().titulo);
+  //addCommentElement(postElement, data.key, data.val().text, data.val().author);
+});
+
+
+/*
+var notificacion="noty1";
+var starCountRef = database.ref('notificaciones/' + notificacion);
+starCountRef.on('value', (snapshot) => {
+  const data = snapshot.val();
+  console.log(data);
+  //updateStarCount(postElement, data);
+});
+*/
